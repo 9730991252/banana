93c50b67dc04c0bcf89d44a09320abab66826872
9730991252 <sunilkale101@gmail.com>
2024-11-21 18:23:58 +0530

    ok

--- office/urls.py
+++ office/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('new_farmer_bill/', views.new_farmer_bill, name='new_farmer_bill'),
     path('farmer_bill/', views.farmer_bill, name='farmer_bill'),
     path('view_farmer_bill/<int:id>', views.view_farmer_bill, name='view_farmer_bill'),
+    path('add_employee/', views.add_employee, name='add_employee'),
+    path('profile/', views.profile, name='profile'),
 ]
--- office/views.py
+++ office/views.py
@@ -108,3 +108,87 @@ def view_farmer_bill(request, id):
         return render(request, 'office/view_farmer_bill.html', context)
     else:
         return redirect('login')
+    
+def add_employee(request):
+    if request.session.has_key('office_mobile'):
+        mobile = request.session['office_mobile']
+        e = office_employee.objects.filter(mobile=mobile).first()
+        if e:
+            if 'Add_employee'in request.POST:
+                name = request.POST.get('name')
+                mobile = request.POST.get('mobile')
+                pin = request.POST.get('pin')
+                if office_employee.objects.filter(mobile=mobile).exists():
+                    pass
+                else:
+                    office_employee(
+                        shope_id=e.shope.id,
+                        name=name,
+                        mobile=mobile,
+                        pin=pin,
+                    ).save()    
+                return redirect('/owner/add_employee/')
+            if 'Edit_employee'in request.POST:
+                id = request.POST.get('id')
+                name = request.POST.get('name')
+                mobile = request.POST.get('mobile')
+                pin = request.POST.get('pin')
+                office_employee(
+                    id=id,
+                    shope_id=e.shope.id,
+                    name=name,
+                    mobile=mobile,
+                    pin=pin,
+                ).save()
+                return redirect('/office/add_employee/')
+            if 'active'in request.POST:
+                id = request.POST.get('id')
+                c = office_employee.objects.filter(id=id).first()
+                c.status = 0
+                c.save()
+                return redirect('/office/add_employee/')
+            if 'deactive'in request.POST:
+                id = request.POST.get('id')
+                c = office_employee.objects.filter(id=id).first()
+                c.status = 1
+                c.save()
+                return redirect('/office/add_employee/')
+            
+        else:
+            del request.session['office_mobile']
+            return redirect('login')
+        context={
+            'e':e,
+            'office_employee':office_employee.objects.filter(shope_id=e.shope.id)
+        }
+        return render(request, 'office/add_employee.html', context)
+    else:
+        return redirect('login')
+    
+def profile(request):
+    if request.session.has_key('office_mobile'):
+        mobile = request.session['office_mobile']
+        e = office_employee.objects.filter(mobile=mobile).first()
+        if e:
+            if 'edit_profile'in request.POST:
+                shope_name = request.POST.get('shope_name')
+                owner_name = request.POST.get('owner_name')
+                address = request.POST.get('address')
+                description = request.POST.get('description')
+                contact_details = request.POST.get('contact_details')
+                e.shope.shope_name = shope_name
+                e.shope.owner_name = owner_name
+                e.shope.address = address
+                e.shope.description = description
+                e.shope.contact_details = contact_details
+                e.shope.save()
+
+        else:
+            del request.session['office_mobile']
+            
+        context={
+            'e':e
+        }
+        return render(request, 'office/profile.html', context)
+    else:
+        return redirect('login')
